fcfbbe3720f52ea5eedc05a8aeb46863f6355f6f
SergeMax <maxime.cansell@gmail.com>
2020-06-26 11:03:13 +0200

    ok

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -32,8 +33,11 @@ class Home extends StatefulWidget {
 class _Home extends State {
   @override
   Widget build(BuildContext context) {
+
+    var largeur = MediaQuery.of(context).size.width;
     // TODO: implement build
     return new Scaffold(
+      backgroundColor: Color.fromRGBO(255, 199, 0, 1),
       appBar: new AppBar(
         title: new Text("Yellow'Doubt"),
         backgroundColor: Color(0xffFFD12C),
@@ -54,36 +58,77 @@ class _Home extends State {
               }),
         ],
       ),
-      body: new Container(
+      body: new Center(
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            new Container(
 
-        margin: EdgeInsets.only(top: 5, bottom: 5, left: 5, right: 5),
-        decoration: new BoxDecoration(
-            color: Colors.white,
-            borderRadius: new BorderRadius.only(
-              topLeft: const Radius.circular(10.0),
-              topRight: const Radius.circular(10.0),
-              bottomLeft: const Radius.circular(10.0),
-              bottomRight: const Radius.circular(10.0),
-            )
-        ),
-        child: new Center(
-          child: new Card(
-            elevation: 5.0,
-           // color: Colors.white,
-            child: new Container(
-              width: MediaQuery.of(context).size.width /1.08,
-              height: MediaQuery.of(context).size.height /5,
-              child: Image.network(
-                'https://picsum.photos/250?image=9',
-              )
+              height: MediaQuery.of(context).size.width / 10,
+              margin: EdgeInsets.only(top: 20),
+              child: new Text(
+              'Salut les codeurs',
+
+              style : new TextStyle(
+                color: Colors.grey[900],
+                fontSize: 30.0,
+                fontStyle: FontStyle.italic,
+              ),
             ),
+        ),
+            new Card(
+              elevation: 10,
+              child :  new Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: 200,
+                child: new Image.asset(
+                    'images/voielacte.jpg',
+                     fit: BoxFit.cover,
+                ),
 
-          )
+              )
+
+            ),
+            new IconButton(
+                icon: new Icon(Icons.add_box),
+                onPressed: () {
+                  print("Bouton pressé");
+                }),
+            new Container(
+              height: MediaQuery.of(context).size.width / 8,
+              color: Colors.white60,
+              margin: EdgeInsets.only(left: 20, right: 20, bottom: 20),
+              child: new Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  new Icon(
+                    Icons.accessibility,
+                  color: Colors.grey[300],
+                  size: largeur /13,
+                  ),
+                  new Icon(
+                    Icons.account_box,
+                    color: Colors.grey[300],
+                    size: largeur /13,
+                  ),
+                  new Icon(
+                    Icons.account_circle,
+                    color: Colors.grey[300],
+                    size: largeur /13,
+                  ),
+                  new Icon(
+                    Icons.accessible,
+                    color: Colors.grey[300],
+                    size: largeur /13,
+                  ),
+                ],
+              ),
+            )
+          ],
         ),
 
       ),
 
-      backgroundColor: Color.fromRGBO(255, 199, 0, 1),
 
     );
   }
